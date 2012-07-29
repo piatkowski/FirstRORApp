74a6419658d01a50b2d2ecf812bc6a72ae05c884
Krzysztof <piatkowski92@gmail.com>
2012-07-29 20:53:38 +0200

    Dodaję projekt do Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstRORApp::Application.config.secret_token = '6647f1b2c4961ca5267ebd3585f2b4d0c47080fc86d7c33545e6ec3afbb41eda3380eb62c1855d2dedc085a75ed9406dcded7a575d79bd2016f5b8baf341afb8'
